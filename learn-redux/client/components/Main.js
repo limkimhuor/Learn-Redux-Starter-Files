8bb3b167c239b27d30b05a6315a90f63d27a03d9
limkimhuor <kimhuorlim@gmail.com>
2018-06-27 10:44:06 +0700

    Create Signle and PhotoGrid component

--- learn-redux/client/components/Main.js
+++ learn-redux/client/components/Main.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router';
+import Single from './Single';
 
 const Main = React.createClass({
   render() {
     return (
       <div>
         <h1>
-          <Link>Reduxstagram</Link>
+          <Link to="/">Reduxstagram</Link>
         </h1>
+        {React.cloneElement(this.props.children, this.props)}
       </div>
     )
   }
